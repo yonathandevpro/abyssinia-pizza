56ffdbb1e6e08af94fea9b7d81b404330b270e20
yonathandevpro <yonathanber@icloud.com>
2024-08-29 16:15:12 +0300

    Styled the loader component with perspective of the page

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,14 @@
 function Loader() {
-  return <div className="loader"></div>;
+  /*
+      inset-0: sets top, bottom, right, left position to 0. 
+      /20 - puts 20% opacity to the background.
+      backdrop-blur - puts some blurring to the background.
+  */
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>;
+    </div>
+  );
 }
 
 export default Loader;
