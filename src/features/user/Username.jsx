33b39716e47e88d36050e7cda864b2a9ff797178
yonathandevpro <yonathanber@icloud.com>
2024-08-29 21:04:53 +0300

    Modelling the user state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,10 @@
+import { useSelector } from 'react-redux';
+
 export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Yonathan</div>;
+  const userName = useSelector((state) => state.user.username);
+
+  if (!userName) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
