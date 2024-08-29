c376ae5c0b543eb99f7f482f4abec181a2e7fdf2
yonathandevpro <yonathanber@icloud.com>
2024-08-29 21:24:04 +0300

    Reading and updating the user state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,6 +39,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === 'submitting';
   // const [withPriority, setWithPriority] = useState(false);
   const formErrors = useActionData();
+  const username = useSelector((state) => state.user.username);
   // Since createOrder and the createOrderAction are connected through the route, this function has access to the data that is returned from the action function through useActionData()
 
   const cart = fakeCart;
@@ -52,7 +54,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            defaultValue={username}
+            className="input grow"
+            type="text"
+            name="customer"
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
